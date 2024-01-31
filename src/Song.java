5ad31177366a55a97387c0570dd9708e338968ad
tywu999 <tszyengw2026@student.cis.edu.hk>
2024-01-31 10:01:00 +0800

    DONe

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,40 @@
+import java.util.*;
+
 public class Song {
 
-    private String
+    private String title = "";
+    private String artist = "";
+    private ArrayList<String> list;
+
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+        this.list = new ArrayList<>(Arrays.asList("abc", "sdf"));
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    public int howMany(ArrayList<String> listener){
+        int newListeners = 0;
+        for(int i = 0; i < listener.size(); i++){
+            String z = (listener.get(i)).toLowerCase();
+            if(!list.contains(z)){
+                list.add(z);
+                newListeners++;
+            }
+        }
+        return newListeners;
+    }
 }
